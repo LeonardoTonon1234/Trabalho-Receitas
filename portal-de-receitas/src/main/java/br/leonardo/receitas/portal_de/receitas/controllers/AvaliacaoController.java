2f078e40e78f9a93707ca9fff2e254f3980c2b58
LeonardoTonon1234 <146954166+LeonardoTonon1234@users.noreply.github.com>
2024-11-04 10:58:26 +0000

    Commit 8

--- portal-de-receitas/src/main/java/br/leonardo/receitas/portal_de/receitas/controllers/AvaliacaoController.java
+++ portal-de-receitas/src/main/java/br/leonardo/receitas/portal_de/receitas/controllers/AvaliacaoController.java
@@ -1,3 +1,8 @@
+//Feito Por: 
+// Leonardo De Castro Tonon Ra: 10426930
+//MATHEUS CALEIRO PINHEIRO RA: 10418688
+//JOAO PEDRO FERNANDES MILHOMENS RA: 10417578
+
 package br.leonardo.receitas.portal_de.receitas.controllers;
 
 // Importa as classes necessárias para o funcionamento do controlador
--- portal-de-receitas/src/main/java/br/leonardo/receitas/portal_de/receitas/controllers/UsuarioController.java
+++ portal-de-receitas/src/main/java/br/leonardo/receitas/portal_de/receitas/controllers/UsuarioController.java
@@ -1,3 +1,8 @@
+//Feito Por: 
+// Leonardo De Castro Tonon Ra: 10426930
+//MATHEUS CALEIRO PINHEIRO RA: 10418688
+//JOAO PEDRO FERNANDES MILHOMENS RA: 10417578
+
 package br.leonardo.receitas.portal_de.receitas.controllers;
 
 import br.leonardo.receitas.portal_de.receitas.entidades.Usuario; // Importa a entidade Usuario
